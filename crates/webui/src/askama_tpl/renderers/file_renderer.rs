0a9f6a4d5628081f93bdb8b141a7235efb3c8dc4
Frank Huang <me@fyhuang.com>
2025-08-11 16:45:09 -0700

    Abstract RawFile and GeneratedFile into ServableFileRenderer

--- crates/webui/src/askama_tpl/renderers/file_renderer.rs
+++ crates/webui/src/askama_tpl/renderers/file_renderer.rs
@@ -1,5 +1,7 @@
 use serde::Serialize;
 
+use mtk::file_tree;
+
 #[derive(Clone, Serialize)]
 pub struct RawFileRenderer {
     pub repo_path: String,
@@ -8,5 +10,43 @@ pub struct RawFileRenderer {
 impl RawFileRenderer {
     pub fn url(&self) -> String {
         super::super::urlencode_parts(&format!("/raw/{}", self.repo_path))
+    }
+}
+
+#[derive(Clone, Serialize)]
+pub struct GeneratedFileRenderer {
+    pub entry_id: i64,
+    pub file_type_2l: String,
+    pub metadata: String,
+    pub extension: String,
+}
+
+impl GeneratedFileRenderer {
+    pub fn new(gfile: &file_tree::GeneratedFile) -> GeneratedFileRenderer {
+        GeneratedFileRenderer {
+            entry_id: gfile.entry_id,
+            file_type_2l: gfile.file_type.to_two_letter_code().to_string(),
+            metadata: gfile.metadata.clone(),
+            extension: gfile.extension.clone(),
+        }
+    }
+
+    pub fn url(&self) -> String {
+        format!("/generated/{}/{}/{}/{}", self.entry_id, self.file_type_2l, self.metadata, self.extension)
+    }
+}
+
+#[derive(Clone, Serialize)]
+pub enum ServableFileRenderer {
+    RawFile(RawFileRenderer),
+    GeneratedFile(GeneratedFileRenderer),
+}
+
+impl ServableFileRenderer {
+    pub fn url(&self) -> String {
+        match self {
+            ServableFileRenderer::RawFile(rf) => rf.url(),
+            ServableFileRenderer::GeneratedFile(gf) => gf.url(),
+        }
     }
 }
